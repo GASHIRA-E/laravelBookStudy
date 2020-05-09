4b678bc74f775a5e1134bc291fdeb2f1eb1c1a6c
GASHIRA-E <dsteams@outlook.jp>
2020-05-09 18:15:22 +0900

    validationのエラー表示

--- server/resources/views/hello/index.blade.php
+++ server/resources/views/hello/index.blade.php
@@ -9,21 +9,46 @@
 
 @section('content')
   <p>{{$msg}}</p>
+  @if (count($errors) > 0)
+    <p>入力に問題があります。再入力してください。</p>
+  @endif
   <form action="/hello" method="POST">
     <table>
       @csrf
+
+      @if ($errors->has('name'))
+          <tr>
+            <th>ERROR: </th>
+            <td>{{ $errors->first('name') }}</td>
+          </tr>
+      @endif
       <tr>
         <th>name: </th>
         <td><input type="text" name="name"></td>
       </tr>
+
+      @if ($errors->has('mail'))
+        <tr>
+          <th>ERROR: </th>
+          <td>{{ $errors->first('mail') }}</td>
+        </tr>
+      @endif
       <tr>
         <th>mail: </th>
-        <td><input type="text" name="email"></td>
+        <td><input type="text" name="mail"></td>
       </tr>
+
+      @if ($errors->has('age'))
+        <tr>
+          <th>ERROR: </th>
+          <td>{{ $errors->first('age') }}</td>
+        </tr>
+      @endif
       <tr>
         <th>age: </th>
         <td><input type="text" name="age"></td>
       </tr>
+
       <tr>
         <th></th>
         <td><input type="submit" value="send"></td>
